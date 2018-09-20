956155f7b4b11d24d25141e55fada37a9e6137a9
2384152118 <2384152118@qq.com>
2018-09-20 10:44:42 +0800

    表

---
+++ 数据库名  bookstore/admin.sql
@@ -0,0 +1,3 @@
+INSERT INTO `admin`(`adminid`, `adminname`, `adminpassword`) VALUES (1, '管理员1', '123456');
+INSERT INTO `admin`(`adminid`, `adminname`, `adminpassword`) VALUES (2, '管理员2', '123456');
+INSERT INTO `admin`(`adminid`, `adminname`, `adminpassword`) VALUES (3, '管理员3', '123456');
